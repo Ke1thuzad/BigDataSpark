23ddf2221e889c6d997358be09a028a602a40d37
Dmitriy <dolbus1235@mail.ru>
2026-05-03 16:19:44 +0300

    finished

--- init/01_init.sql
+++ init/01_init.sql
@@ -52,22 +52,21 @@ CREATE TABLE public.mock_data (
 );
 
 CREATE TABLE dim_countries (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     name varchar(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE dim_pet_categories (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     category varchar(50)
 );
 
 CREATE TABLE dim_customers (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     customer_first_name varchar(50) NULL,
     customer_last_name varchar(50) NULL,
     customer_age int4 NULL,
     customer_email varchar(50) NULL,
-    customer_country varchar(50) NULL,
     customer_postal_code varchar(50) NULL,
     customer_pet_type varchar(50) NULL,
     customer_pet_name varchar(50) NULL,
@@ -78,42 +77,39 @@ CREATE TABLE dim_customers (
 );
 
 CREATE TABLE dim_stores (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     store_name varchar(50) NULL,
     store_location varchar(50) NULL,
     store_city varchar(50) NULL,
     store_state varchar(50) NULL,
-    store_country varchar(50) NULL,
     store_phone varchar(50) NULL,
     store_email varchar(50) NULL,
     country_id int4 NULL REFERENCES dim_countries
 );
 
 CREATE TABLE dim_suppliers (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     supplier_name varchar(50) NULL,
     supplier_contact varchar(50) NULL,
     supplier_email varchar(50) NULL,
     supplier_phone varchar(50) NULL,
     supplier_address varchar(50) NULL,
     supplier_city varchar(50) NULL,
-    supplier_country varchar(50) NULL,
     country_id int4 NULL REFERENCES dim_countries
 );
 
 CREATE TABLE dim_sellers (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     seller_first_name varchar(50) NULL,
     seller_last_name varchar(50) NULL,
     seller_email varchar(50) NULL,
-    seller_country varchar(50) NULL,
     seller_postal_code varchar(50) NULL,
     seller_store_id int4 NULL REFERENCES dim_stores,
     country_id int4 NULL REFERENCES dim_countries
 );
 
 CREATE TABLE dim_products (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     product_name varchar(50) NULL,
     product_category varchar(50) NULL,
     product_price float4 NULL,
@@ -132,22 +128,13 @@ CREATE TABLE dim_products (
 );
 
 CREATE TABLE fact_sales (
-    id serial PRIMARY KEY,
+    id int4 PRIMARY KEY,
     sale_date varchar(50) NULL,
     sale_customer_id int4 NULL REFERENCES dim_customers,
     sale_seller_id int4 NULL REFERENCES dim_sellers,
     sale_product_id int4 NULL REFERENCES dim_products,
+    sale_store_id int4 NULL REFERENCES dim_stores,
+    sale_supplier_id int4 NULL REFERENCES dim_suppliers,
     sale_quantity int4 NULL,
     sale_total_price float4 NULL
 );
-
-COPY mock_data FROM '/data/MOCK_DATA.csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (1).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (2).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (3).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (4).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (5).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (6).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (7).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (8).csv' WITH (FORMAT CSV, HEADER TRUE);
-COPY mock_data FROM '/data/MOCK_DATA (9).csv' WITH (FORMAT CSV, HEADER TRUE);
